2b9b56b3d078c237505b4eb82e37afe2ddd3df91
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-01-19 18:25:45 +0530

    Fixed erlang punctuation

--- src/wrinq_login_user.erl
+++ src/wrinq_login_user.erl
@@ -32,7 +32,7 @@ login_user(Details,Req,State)->
     {ok, User} = eredis:q(State,["HGET",User_Name,<<"p">>]),
     try
 	{[{<<"n">>,User_Name},{<<"p">>,Password},{<<"id">>,Id}]} = jiffy:decode(User),
-	P = jiffy:encode({[{Id,User_Name}]})
+	P = jiffy:encode({[{Id,User_Name}]}),
 	cowboy_req:reply(200,[],P,Req)
     catch
 	_:_-> cowboy_req:reply(404,Req)
